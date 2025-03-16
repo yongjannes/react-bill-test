9a6d97c3772d8d6ecf344fe85cc75c8441f820fc
yongjannes <2380444947@qq.com>
2025-03-16 16:14:32 +0800

    使用RTK管理账单列表

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import { Provider } from 'react-redux'
+import store from './store'
 
 // 导入定制主题文件
 import './theme.css'
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Provider store={store}>
     <RouterProvider router={router} />
+    </Provider>
 );
