ff31c964aed73e853138cd03fb5bb844add9bad4
yongjannes <2380444947@qq.com>
2025-03-16 15:51:55 +0800

    主题色的配置

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 
+// 导入定制主题文件
+import './theme.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <RouterProvider router={router} />
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
-
+import { Button } from "antd-mobile";
 const Layout = () => {
     return (
         <div>
             <Outlet />
-            我是Layout</div>
+            我是Layout
+            {/* 测试全局生效样式 */}
+            <Button color="primary">测试全局</Button>
+            <div className="puple"> <Button color="primary">测试局部</Button></div>
+        </div>
     )
 }
 
