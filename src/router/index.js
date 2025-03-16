862912df8c0886d1bf5c03226dacbe48f2807ef7
yongjannes <2380444947@qq.com>
2025-03-16 15:26:34 +0800

    整体路由设计配置

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+// 创建路由实例 绑定path Element
+
+import Layout from '@/pages/Layout'
+import Month from '@/pages/Month'
+import New from '@/pages/New'
+import Year from '@/pages/Year'
+import {createBrowserRouter } from 'react-router-dom'
+
+
+const router = createBrowserRouter ([
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                // path: 'month',
+                index: true,
+                element: <Month />
+            },
+            {
+                path: 'year',
+                element: <Year />
+            },
+        ]
+    },
+    {
+        path: '/new',
+        element: <New />
+    }
+
+])
+export default router
